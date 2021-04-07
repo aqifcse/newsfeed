bb803313c175229dd2f6e957e2f832693470c1e7
Syed Towfiqur Rahim <5446400-aqifcse@users.noreply.gitlab.com>
2021-04-08 03:51:53 +0600

    1. Added Necessary Templates 2. Setup Admin, Model Config 3. Added Neccessary view functions for the templates 4. Completed User Prodile Updating feature

--- newsfeed_portal/portal/urls.py
+++ newsfeed_portal/portal/urls.py
@@ -6,7 +6,12 @@ app_name = 'portal'
 urlpatterns = [
     path('aljazeera/', AlJazeera, name = 'AlJazeera'),
     path('bbc/', BBC, name = 'BBC'),
+    
     path('user-home/', UserHomeView.as_view(), name='user_home'),
+    path('user-country-based-news/', UserCountryBasedNewsView.as_view(), name='user_country_based_news'),
+    path('user-source-based-news/', UserSourceBasedNewsView.as_view(), name='user_source_based_news'),
+    path('user-keyword-based-news/', UserKeywordBasedNewsView.as_view(), name='user_keyword_based_news'),
+
     path('profile/', profile, name='profile'),
     path('settings/', SettingsView.as_view(), name='settings'),
 ]
